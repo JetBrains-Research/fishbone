f1194e42c9d4d1f640a7e6e7562f55a78cb0be59
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2019-07-04 11:31:00 +0300

    PredicatesInjector or/and control

--- src/main/kotlin/org/jetbrains/bio/rules/PredicatesInjector.kt
+++ src/main/kotlin/org/jetbrains/bio/rules/PredicatesInjector.kt
@@ -5,20 +5,21 @@ import org.jetbrains.bio.predicates.*
 import java.util.function.Consumer
 
 /**
- * Inserts atomic predicates inside given predicate at all possible places
+ * Inserts predicates inside given predicate at all possible places
  */
 object PredicatesInjector {
 
-    fun <T> injectPredicate(predicate: Predicate<T>, predicate2Inject: Predicate<T>): Collection<Predicate<T>> {
+    fun <T> injectPredicate(predicate: Predicate<T>, predicate2Inject: Predicate<T>,
+                            and: Boolean = true,
+                            or: Boolean = true): Collection<Predicate<T>> {
         val result = Sets.newHashSet<Predicate<T>>()
-        predicate.accept(createInjector(setOf(predicate2Inject), Consumer { result.add(it) }))
+        predicate.accept(createInjector(setOf(predicate2Inject), and, or, Consumer { result.add(it) }))
         return result
     }
 
-    /**
-     * Adds atomic at all possible positions of given formula
-     */
     private fun <T> createInjector(atomics: Collection<Predicate<T>>,
+                                   and: Boolean = true,
+                                   or: Boolean = true,
                                    consumer: Consumer<Predicate<T>>): PredicateVisitor<T> {
         return object : PredicateVisitor<T>() {
             override fun visitAndPredicate(predicate: AndPredicate<T>) {
@@ -28,7 +29,7 @@ object PredicatesInjector {
                     // Replace operand
                     val woIndex = predicate.remove(i)
                     operands[i].accept(
-                            createInjector(atomics,
+                            createInjector(atomics, and, or,
                                     Consumer { newPredicate -> consumer.accept(woIndex.and(newPredicate)) }))
                 }
             }
@@ -40,7 +41,7 @@ object PredicatesInjector {
                     // Replace operand
                     val woIndex = predicate.remove(i)
                     operands[i].accept(
-                            createInjector(atomics,
+                            createInjector(atomics, and, or,
                                     Consumer { newPredicate -> consumer.accept(woIndex.or(newPredicate)) }))
                 }
             }
@@ -52,7 +53,7 @@ object PredicatesInjector {
 
             override fun visitParenthesisPredicate(predicate: ParenthesesPredicate<T>) {
                 process(predicate)
-                predicate.operand.accept(createInjector(atomics, Consumer { consumer.accept(it) }))
+                predicate.operand.accept(createInjector(atomics, and, or, Consumer { consumer.accept(it) }))
             }
 
             override fun visit(predicate: Predicate<T>) {
@@ -61,11 +62,14 @@ object PredicatesInjector {
 
             private fun process(predicate: Predicate<T>) {
                 atomics.forEach {
-                    consumer.accept(predicate.or(it))
-                    consumer.accept(predicate.and(it))
+                    if (or) {
+                        consumer.accept(predicate.or(it))
+                    }
+                    if (and) {
+                        consumer.accept(predicate.and(it))
+                    }
                 }
             }
         }
     }
-
 }
