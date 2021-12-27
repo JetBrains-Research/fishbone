19bfd796c37efe37a33555f6d2b0cdcadd0a385b
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-12-27 17:36:48 +0300

    RulesLoggerTest - make test deterministic

--- src/test/kotlin/org/jetbrains/bio/fishbone/rule/log/RulesLoggerTest.kt
+++ src/test/kotlin/org/jetbrains/bio/fishbone/rule/log/RulesLoggerTest.kt
@@ -6,6 +6,8 @@ import org.jetbrains.bio.fishbone.miner.RangePredicate
 import org.jetbrains.bio.fishbone.miner.named
 import org.jetbrains.bio.fishbone.predicate.FalsePredicate
 import org.jetbrains.bio.fishbone.rule.Rule
+import org.jetbrains.bio.util.bufferedWriter
+import org.jetbrains.bio.util.toPath
 import org.junit.Test
 import java.awt.Color
 
@@ -18,189 +20,189 @@ class RulesLoggerTest {
 
     @Test
     fun testGetJson() {
-        val predicates = 0.until(2).map { RangePredicate(it * 10, (it + 1) * 10) }
+        val predicates = 0.until(2).map { RangePredicate(0, (it + 1) * 10) }
         val database = 0.until(50).toList()
         val logger = RulesLogger(null)
-        FishboneMiner.mine("foo", database, listOf(predicates to RangePredicate(0, 20)),
+        FishboneMiner.mine(
+            "foo", database, listOf(predicates to RangePredicate(10, 20)),
             { logger.log("id", it) }, maxComplexity = 2, topPerComplexity = 1, buildHeatmapAndUpset = true
         )
         val actualMap = logger.prepareJson({ Color.WHITE }, "conviction")
-        val expectedMap = RulesLogger.GSON.fromJson("""
+        val expectedMap = RulesLogger.GSON.fromJson("""{
+  "records": [
     {
-      "records": [
-        {
-          "id": "id",
-          "condition": "[0;10) OR [10;20)",
-          "target": "[0;20)",
-          "database_count": 50,
-          "condition_count": 20,
-          "target_count": 20,
-          "intersection_count": 20,
-          "support": 0.4,
-          "confidence": 1.0,
-          "correlation": 1.0,
-          "lift": 2.5,
-          "conviction": 12.0,
-          "loe": 1.4274698120945677,
-          "complexity": 2,
-          "node": "[0;10)",
-          "parent_node": "[10;20)",
-          "parent_condition": "[10;20)",
-          "aux": {
-            "rule": {
-              "names": [
-                "[0;10)",
-                "[10;20)",
-                "[0;20)"
-              ],
-              "combinations": [
-                30,
-                0,
-                0,
-                0,
-                0,
-                10,
-                10,
-                0
-              ]
-            }
-          },
-          "operator": "or"
-        },
-        {
-          "id": "id",
-          "condition": "[10;20)",
-          "target": "[0;20)",
-          "database_count": 50,
-          "condition_count": 10,
-          "target_count": 20,
-          "intersection_count": 10,
-          "support": 0.2,
-          "confidence": 1.0,
-          "correlation": 0.6123724356957945,
-          "lift": 2.5,
-          "conviction": 6.0,
-          "loe": 1.2007703985251723,
-          "complexity": 1,
-          "node": "[10;20)",
-          "aux": {
-            "rule": {
-              "names": [
-                "[10;20)",
-                "[0;20)"
-              ],
-              "combinations": [
-                30,
-                0,
-                10,
-                10
-              ]
-            }
-          },
-          "operator": "none"
-        },
-        {
-          "id": "id",
-          "condition": "TRUE",
-          "target": "[0;20)",
-          "database_count": 50,
-          "condition_count": 50,
-          "target_count": 20,
-          "intersection_count": 20,
-          "support": 1.0,
-          "confidence": 0.4,
-          "correlation": 0.0,
-          "lift": 1.0,
-          "conviction": 0.967741935483871,
-          "loe": 0.20827504596683347,
-          "complexity": 1,
-          "node": "TRUE",
-          "aux": {
-            "heatmap": {
-              "tableData": [
-                {
-                  "key": "[0;20)",
-                  "values": [
-                    {
-                      "key": "[0;20)",
-                      "value": 1.0
-                    },
-                    {
-                      "key": "[10;20)",
-                      "value": 0.6123724356957945
-                    }
-                  ]
-                },
+      "id": "id",
+      "condition": "NOT [0;10) AND [0;20)",
+      "target": "[10;20)",
+      "database_count": 50,
+      "condition_count": 10,
+      "target_count": 10,
+      "intersection_count": 10,
+      "support": 0.2,
+      "confidence": 1,
+      "correlation": 1,
+      "lift": 5,
+      "conviction": 8,
+      "loe": 1.151802008640984,
+      "complexity": 2,
+      "node": "NOT [0;10)",
+      "parent_node": "[0;20)",
+      "parent_condition": "[0;20)",
+      "aux": {
+        "rule": {
+          "names": [
+            "NOT [0;10)",
+            "[0;20)",
+            "[10;20)"
+          ],
+          "combinations": [
+            0,
+            30,
+            10,
+            0,
+            0,
+            0,
+            0,
+            10
+          ]
+        }
+      },
+      "operator": "and"
+    },
+    {
+      "id": "id",
+      "condition": "[0;20)",
+      "target": "[10;20)",
+      "database_count": 50,
+      "condition_count": 20,
+      "target_count": 10,
+      "intersection_count": 10,
+      "support": 0.4,
+      "confidence": 0.5,
+      "correlation": 0.6123724356957945,
+      "lift": 2.5,
+      "conviction": 1.4545454545454546,
+      "loe": 0.48718084308604387,
+      "complexity": 1,
+      "node": "[0;20)",
+      "aux": {
+        "rule": {
+          "names": [
+            "[0;20)",
+            "[10;20)"
+          ],
+          "combinations": [
+            30,
+            10,
+            0,
+            10
+          ]
+        }
+      },
+      "operator": "none"
+    },
+    {
+      "id": "id",
+      "condition": "TRUE",
+      "target": "[10;20)",
+      "database_count": 50,
+      "condition_count": 50,
+      "target_count": 10,
+      "intersection_count": 10,
+      "support": 1,
+      "confidence": 0.2,
+      "correlation": 0,
+      "lift": 1,
+      "conviction": 0.975609756097561,
+      "loe": 0.057131853609317905,
+      "complexity": 1,
+      "node": "TRUE",
+      "aux": {
+        "heatmap": {
+          "tableData": [
+            {
+              "key": "[10;20)",
+              "values": [
                 {
                   "key": "[10;20)",
-                  "values": [
-                    {
-                      "key": "[0;20)",
-                      "value": 0.6123724356957945
-                    },
-                    {
-                      "key": "[10;20)",
-                      "value": 1.0
-                    }
-                  ]
+                  "value": 1
+                },
+                {
+                  "key": "[0;20)",
+                  "value": 0.6123724356957945
                 }
-              ],
-              "rootData": {
-                "totalLength": 1.5142135623730952,
-                "children": [
-                  {
-                    "length": 1.4142135623730951,
-                    "key": "[0;20)"
-                  },
-                  {
-                    "length": 1.4142135623730951,
-                    "key": "[10;20)"
-                  }
-                ]
-              }
+              ]
             },
-            "upset": {
-              "names": [
-                "[0;20)",
-                "[10;20)"
-              ],
-              "data": [
+            {
+              "key": "[0;20)",
+              "values": [
                 {
-                  "id": [
-                    0
-                  ],
-                  "n": 20
+                  "key": "[10;20)",
+                  "value": 0.6123724356957945
                 },
                 {
-                  "id": [
-                    0,
-                    1
-                  ],
-                  "n": 10
-                },
-                {
-                  "id": [
-                    1
-                  ],
-                  "n": 10
+                  "key": "[0;20)",
+                  "value": 1
                 }
               ]
             }
-          },
-          "operator": "none"
+          ],
+          "rootData": {
+            "totalLength": 1.5142135623730952,
+            "children": [
+              {
+                "length": 1.4142135623730951,
+                "key": "[10;20)"
+              },
+              {
+                "length": 1.4142135623730951,
+                "key": "[0;20)"
+              }
+            ]
+          }
+        },
+        "upset": {
+          "names": [
+            "[10;20)",
+            "[0;20)"
+          ],
+          "data": [
+            {
+              "id": [
+                0
+              ],
+              "n": 10
+            },
+            {
+              "id": [
+                0,
+                1
+              ],
+              "n": 10
+            },
+            {
+              "id": [
+                1
+              ],
+              "n": 20
+            }
+          ]
         }
-      ],
-      "palette": {
-        "TRUE": "#ffffff",
-        "[0;10)": "#ffffff",
-        "[0;20)": "#ffffff",
-        "[10;20)": "#ffffff"
       },
-      "criterion": "conviction"
-    }""", Map::class.java)
-        val expected = RulesLogger.GSON.toJson(expectedMap)
-        val actual = RulesLogger.GSON.toJson(actualMap)
-        assertEquals(expected.replace(".0", ""), actual.replace(".0", ""))
+      "operator": "none"
+    }
+  ],
+  "palette": {
+    "TRUE": "#ffffff",
+    "[0;10)": "#ffffff",
+    "[0;20)": "#ffffff",
+    "[10;20)": "#ffffff"
+  },
+  "criterion": "conviction"
+}""", Map::class.java)
+        val expected = RulesLogger.GSON.fromJson(RulesLogger.GSON.toJson(expectedMap), Map::class.java)
+        val actual = RulesLogger.GSON.fromJson(RulesLogger.GSON.toJson(actualMap), Map::class.java)
+        assertEquals(expected, actual)
     }
 
 
